e9321d1a5c37f6f138c4fd8d3487331bef6211d3
Juanjo García <ju_ga_da@hotmail.com>
2016-03-14 08:54:30 -0100

    [SF][SETUP] Initial project deploy "Examen" * He creado un directorio src/MVC y dentro el archivo ModuleBundle.php siguiendo las instrucciones de http://symfony.com/doc/2.7/book/bundles.html#creating-a-bundle * He hecho la instrucción "php app/console generate:bundle --namespace=src\MVC\ModuleBundle" * He añadido los archivos generados al commit

---
+++ src/MVC/ModuleBundle.php
@@ -0,0 +1,8 @@
+// src/MVC/ModuleBundle.php
+namespace MVC;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeTestBundle extends Bundle
+{
+}
---
+++ src/srcMVCModuleBundle/srcMVCModuleBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace srcMVCModuleBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class srcMVCModuleBundle extends Bundle
+{
+}
